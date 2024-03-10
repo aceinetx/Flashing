6ebbd0bef3c971d4c992dd54a9c52749ca233cb3
aceinetx <158546973+aceinetx@users.noreply.github.com>
2024-03-10 16:22:40 +0300

    Update flashing.sh

--- flashing.sh
+++ flashing.sh
@@ -1,9 +1,10 @@
+
 echo -e "Delay: "
+#Get requested delay
 read delay
 
 while [ true ]
 do
-
 	echo 1 > /sys/class/leds/flashlight/brightness
 	sleep $delay
 	echo 0 > /sys/class/leds/flashlight/brightness
